125e4d8042159902ffc0c8cc393c7a700f10425a
babysharkdududududududu <thanh89508@gmail.com>
2022-12-18 13:23:12 +0700

    init Project  1

--- src/main/java/ql/ql/QL.java
+++ src/main/java/ql/ql/QL.java
@@ -5,13 +5,30 @@
 
 package ql.ql;
 
+import gui.Gui_DangNhap;
+import gui.Menu;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
 /**
  *
  * @author Admin
  */
 public class QL {
+    public static String random(String x){
+        Random rd = new Random();
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyMMdd");
+        String ma = x + sdf.format(date)  + String.format("%04d", rd.nextInt(1000));
+        return ma;
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(random("NV"));
+        
+//        new Gui_DangNhap().setVisible(true);
+           new Menu().setVisible(true);
     }
 }
